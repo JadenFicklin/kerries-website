979675ed1b25be6f30cfcf8e70a7a00948ec454f
JadenFicklin <jadendficklin@gmail.com>
2022-04-26 12:53:36 -0600

    added section 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,56 @@ function Home() {
     <>
       <Nav />
       <div className="header"></div>
+      <div className="section-two">
+        <div className="section-two-top">
+          <div className="box-one">
+            <div className="inner-one">
+              <span>A Unique Place</span>
+              <span>_</span>
+              <p>
+                Integer diam quam, ultrices non lobortis ut, ultricies at libero
+                phasellus luctus{" "}
+              </p>
+            </div>
+          </div>
+          <div className="box-two"></div>
+          <div className="box-three">
+            <div className="inner-three">
+              <span>Lip Tattoos</span>
+              <span>_</span>
+              <p>
+                Integer diam quam, ultrices non lobortis ut, ultricies at libero
+                phasellus luctus{" "}
+              </p>
+            </div>
+          </div>
+          <div className="box-four"></div>
+        </div>
+        <div className="section-two-bottom">
+          <div className="box-five"></div>
+          <div className="box-six">
+            <div className="inner-six">
+              <span>Tattoos</span>
+              <span>_</span>
+              <p>
+                Integer diam quam, ultrices non lobortis ut, ultricies at libero
+                phasellus luctus{" "}
+              </p>
+            </div>
+          </div>
+          <div className="box-seven"></div>
+          <div className="box-eight">
+            <div className="inner-eight">
+              <span>Makeup</span>
+              <span>_</span>
+              <p>
+                Integer diam quam, ultrices non lobortis ut, ultricies at libero
+                phasellus luctus{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
