79e923f64c5aff04af016c9fb18f97a139c64b31
JadenFicklin <jadendficklin@gmail.com>
2022-04-26 15:03:01 -0600

    added section three of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -57,6 +57,26 @@ function Home() {
           </div>
         </div>
       </div>
+      <div className="section-three">
+        <div className="section-three-left">
+          <div className="portrait-one-box"></div>
+          <div className="portrait-one-outline"></div>
+          <div className="portrait-one"></div>
+        </div>
+        <div className="section-three-right">
+          <span className="who-header">Who I am</span>
+          <p className="who-paragraph">
+            Im a mom to 2 kids, a dog and wife! I enjoy being creative and love
+            to draw up all the tattoo ideas! I started out doing permanent
+            makeup at the beginning of my tattooing career and fell in love with
+            doing lips, so I decided to focus my energy on Lips alone! But I
+            have always felt like I was missing the creative side that artistic
+            tattooing could give me, so I took on some more training and here I
+            am :) I love my job and I love working with people! Send me any and
+            all of your ideas :) Lets see what we can create!
+          </p>
+        </div>
+      </div>
     </>
   );
 }
