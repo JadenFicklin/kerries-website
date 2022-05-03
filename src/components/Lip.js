5b268a1a28c4c062e6f420db9f428939107848ea
JadenFicklin <jadendficklin@gmail.com>
2022-05-03 12:32:54 -0600

    finished styling on lip page

--- src/components/Lip.js
+++ src/components/Lip.js
@@ -51,6 +51,32 @@ function Lip() {
       </div>
       <div className="lip-box-mid"></div>
       <div className="lip-box-mid-line"></div>
+      <div className="lip-portrait-mid"></div>
+      <div className="lip-box-mid-lower"></div>
+      <div className="lip-box-mid-lower-line"></div>
+      <div className="lip-box-bottom">
+        <div className="lip-one">01</div>
+      </div>
+      <div className="lip-one-header">What It’s Like to Get a Lip Tattoo</div>
+      <div className="lip-lower-paragraph">
+        This Service is done using a Rotary Tattoo machine and needle cartridges
+        just like a regular tattoo! I use different needle configurations for
+        different styles of cosmetic lip tattoos, so we can get the look you are
+        going for! I also have over 30 different shades that we can mix and
+        match to get your target color! If you have a favorite lip color, you
+        can bring it with you to your appointment and we can color match! I have
+        also gotten extra training in scar corrections, so if you are concerned
+        about that at all, please contact me and let me see if I can help you
+        out! <br></br>
+        <br></br>Cosmetic lip tattoos are considered semi-permanent. The color
+        will never fully go away, but thanks to our lips skin cells shedding
+        cycles, you may need a color boost every 3 to five years. If you are
+        ever worried about changing your mind on the color you chose, most
+        colors can be corrected. The best time to color correct is at your
+        initial 6-8 week touchup, however if it takes you longer to decide that
+        the color doesnt work for you, I can always get you in for a color
+        adjustment :){" "}
+      </div>
     </>
   );
 }
